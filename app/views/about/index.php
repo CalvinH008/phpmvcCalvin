ea2573284996f3c8c09a0f6ccc8abbd7308f5b28
CalvinH008 <tololcapung@gmail.com>
2025-12-19 00:44:12 +0700

    dengan bootstrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-<h1>about me</h1>
+<div class="container">
+<h1 class="mt-4">about me</h1>
+<img src="<?= BASEURL; ?>/img/IMG-20251202-WA0016[1].jpg" alt="Calvin" width="200" height="200" class="rounded-circle shadow" >
 <p>Nama saya adalah <?= $data['nama'] ?>, umur saya itu <?= $data['umur']?> tahun, 
 status saya masih <?= $data['status'] ?> </p>
+</div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,9 @@
-<h1>Selamat datang</h1>
+<div class="container">
+<div class="bg-light p-5 rounded-lg mt-4">
+    <h1 class="display-4">Selamat Datang di PHPMVC!</h1>
+    <p class="lead">Ini adalah framework PHP MVC sederhana yang saya buat untuk belajar arsitektur MVC.</p>
+    <hr class="my-4">
+    <p>Framework ini menggunakan pattern MVC untuk memisahkan logic, view, dan data.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+</div>
